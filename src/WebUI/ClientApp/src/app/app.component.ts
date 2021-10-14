2620a7bf05aa6fed9ce07c2850a8857187543a93
gangabharat <gangabharat@gmail.com>
2021-10-14 16:01:07 +0530

    components services json & shared json

--- src/WebUI/ClientApp/src/app/app.component.ts
+++ src/WebUI/ClientApp/src/app/app.component.ts
@@ -1,9 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ToastrService } from "ngx-toastr";
+import { HttpClientService } from "./shared/http-client.service";
+import { NotificationService } from "./shared/notification.service";
+import { PushNotificationsService } from "./shared/push-notification.service";
+import { interval, Observable, of, forkJoin, from } from "rxjs";
+import { map, tap } from "rxjs/operators";
+import { ManageHttpRequestService } from "./shared/manage-http-request.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html'
+  selector: "app-root",
+  templateUrl: "./app.component.html",
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = "app";
+
+  public loadData: string[] = [];
+  public isLoading: boolean = false;
+
+  constructor(
+    private httpClient: HttpClientService,
+    private pushNotification: PushNotificationsService,
+    private ts: ToastrService,
+    private notificationService: NotificationService,
+    private httpRequesthandler : ManageHttpRequestService
+  ) {}
+
+  ngOnInit() {
+    // this.loadUsers().then(res =>{
+    //   //console.log(res);
+    // })
+    this.httpRequesthandler.httpLoader.subscribe(
+      (res : boolean )=>{
+        this.isLoading = res;
+      }
+    );
+    this.httpClient.get({url:"https://jsonplaceholder.typicode.com/photos"}).subscribe();
+  }
+
+  // loadUsers(): Promise<any[]> {
+  //   return fetch("https://jsonplaceholder.typicode.com/photos").then((response) => response.json());
+  // }
 }
---
+++ src/WebUI/ClientApp/src/app/service/bible.service.ts
@@ -0,0 +1,37 @@
+import { Injectable, OnInit , EventEmitter} from '@angular/core';
+import { Bible } from '../bible/bible';
+
+import { HttpClientService } from '../shared/http-client.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BibleService {
+  
+  data : Bible;  
+  private _bibleLanguage = "english";
+  private _bibleLanguagePath = "assets/bible/english/bible.json";
+  constructor(private _http: HttpClientService) { }  
+
+  public bibleLanguages: string [] = ["telugu","english","hindi","tamil"];
+  
+  set BibleLanguage(value : string){
+    //let initialPath = `assets/bible/${this.initialLanguage}/bible.json`;
+    let selectedBible = this.bibleLanguages.find(lang => lang == value.toLowerCase());
+    if(selectedBible){
+      this._bibleLanguagePath = `assets/bible/${selectedBible}/bible.json`;
+      this._bibleLanguage = selectedBible;      
+    }
+    //return initialPath;
+  }
+
+  get BibleLanguage(){
+    return this._bibleLanguage;
+  }
+
+  getBible()
+  {    
+    return  this._http.get<Bible>({ url: this._bibleLanguagePath, cacheMins: 0 });    
+  }
+  
+}

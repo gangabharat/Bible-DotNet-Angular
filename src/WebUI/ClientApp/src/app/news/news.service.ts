f0edab54f90e33d36b9d93090a2257dc86ac2a50
gangabharat <gangabharat@gmail.com>
2021-11-04 22:12:56 +0530

    updated news service  load more options

--- src/WebUI/ClientApp/src/app/news/news.service.ts
+++ src/WebUI/ClientApp/src/app/news/news.service.ts
@@ -1,13 +1,13 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-import { News } from './news.model';
+import { Injectable } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
+import { News } from "./news.model";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class NewsService {
-
-  // Make _puppiesSource private so it's not accessible from the outside, 
+  
+  // Make _puppiesSource private so it's not accessible from the outside,
   // expose it as puppies$ observable (read-only) instead.
   // Write to _puppiesSource only through specified store methods below.
   private readonly _puppiesSource = new BehaviorSubject<News[]>([]);
@@ -16,8 +16,11 @@ export class NewsService {
   // Exposed observable (read-only).
   readonly puppies$ = this._puppiesSource.asObservable();
 
+  private puppies: News[] = [];
+  private allPuppies: News[] = [];
+
   constructor() {}
-  
+
   // Get last value without subscribing to the puppies$ observable (synchronously).
   get(): News[] {
     return this._puppiesSource.getValue();
@@ -28,8 +31,13 @@ export class NewsService {
   }
 
   load(puppy: News[]): void {
-    //const puppies = [...this.get(), puppy];
-    this._setPuppies(puppy);
+    this.allPuppies = puppy;
+  }
+
+  loadMore() {
+    if (this.getNextPuppies()) {
+      this._setPuppies(this.puppies);
+    }
   }
 
   add(puppy: News): void {
@@ -38,12 +46,31 @@ export class NewsService {
   }
 
   updatePuppy(puppy: News): void {
-    const puppies = this.get().map(obj => obj.Title === puppy.Title ? puppy : obj);    
-    this._setPuppies(puppies);    
+    const puppies = this.get().map((obj) =>
+      obj.Title === puppy.Title ? puppy : obj
+    );
+    this._setPuppies(puppies);
   }
 
   removePuppy(puppy: News): void {
-    const puppies = this.get().filter(p => p.Title !== puppy.Title);
+    const puppies = this.get().filter((p) => p.Title !== puppy.Title);
     this._setPuppies(puppies);
+  }
+
+  private getNextPuppies(): boolean {
+    if (this.puppies.length >= this.allPuppies.length) {
+      return false;
+    }
+    const remainingLength = Math.min(
+      5,
+      this.allPuppies.length - this.puppies.length
+    );
+    this.puppies.push(
+      ...this.allPuppies.slice(
+        this.puppies.length,
+        this.puppies.length + remainingLength
+      )
+    );
+    return true;
   }
 }

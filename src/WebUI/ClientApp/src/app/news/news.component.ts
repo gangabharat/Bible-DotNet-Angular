b616d05400f73d50b23860873c84b42987da6f9e
gangabharat <gangabharat@gmail.com>
2021-11-04 22:16:24 +0530

    load more included news

--- src/WebUI/ClientApp/src/app/news/news.component.ts
+++ src/WebUI/ClientApp/src/app/news/news.component.ts
@@ -1,28 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { NewsHttpService } from './news-http.service';
-import { News } from './news.model';
-import { NewsService } from './news.service';
+import { Component, OnInit } from "@angular/core";
+import { NewsHttpService } from "./news-http.service";
+import { News } from "./news.model";
+import { NewsService } from "./news.service";
 
 @Component({
-  selector: 'app-news',
-  templateUrl: './news.component.html',
-  styleUrls: ['./news.component.css']
+  selector: "app-news",
+  templateUrl: "./news.component.html",
+  styleUrls: ["./news.component.css"],
 })
 export class NewsComponent implements OnInit {
-
-  newsData : News[] = [];
-  constructor(private newsHttpService : NewsHttpService, private newsService : NewsService) { }
+  newsData: News[] = [];
+  constructor(
+    private newsHttpService: NewsHttpService,
+    private newsService: NewsService
+  ) {}
 
   ngOnInit(): void {
+    this.newsHttpService.getNews().subscribe((res) => {
+      this.newsService.load(res);
+    });
 
-    // this.newsHttpService.getNews().subscribe(
-    //   (res) =>{
-    //       this.newsData = res;          
-    //       //this.newsService.load(res);
-    //   }
-    // );
-
-
+    this.newsService.puppies$.subscribe((res) => {
+      console.log(res);
+    });    
   }
 
+  loadMore() {
+    this.newsService.loadMore();    
+  }
 }

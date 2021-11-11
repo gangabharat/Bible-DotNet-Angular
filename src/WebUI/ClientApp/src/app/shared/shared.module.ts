f3ed4b2948545ba51280853aaeee560cf84f0e7d
gangabharat <gangabharat@gmail.com>
2021-11-11 19:34:45 +0530

    code clean up

--- src/WebUI/ClientApp/src/app/shared/shared.module.ts
+++ src/WebUI/ClientApp/src/app/shared/shared.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-import { ScrollTrackerDirective } from './directive/scroll-tracker.directive';
 
 
 @NgModule({
-  declarations: [ScrollTrackerDirective],
+  declarations: [],
   imports: [
     // FormsModule,
     // BrowserModule       
